32da1fd769c6be0bcc9c8ebdb2fd6dcacefc502d
Mahesh Nadun <mahesh.nandun2220@gmail.com>
2025-11-19 17:42:46 +0530

    java
    java

---
+++ p2.java
@@ -0,0 +1,16 @@
+public class p2{
+    public static void main(String[]args){
+        String name = "Mahesh Nadun";
+        int num = 12;
+        char ch = 'A';
+        double du = 22.23;
+        float f = 4.4333f;
+
+        System.out.println("String" + name);     
+        System.out.println("int" + num);
+        System.out.println("char" + ch);
+        System.out.println("double" + du);
+        System.out.println("float" + f);   
+
+    }
+}
