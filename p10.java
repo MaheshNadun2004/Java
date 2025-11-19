50143472b2b927a1e57dcc83b0a32e9622a1ab73
Mahesh Nadun <mahesh.nandun2220@gmail.com>
2025-11-19 19:41:36 +0530

    java
    java

---
+++ p10.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class p10{
+    public static void main(String[]args){
+        String name;
+        Scanner scen = new Scanner(System.in);
+        System.out.println("Entern name: ");
+        name = scen.nextLine();
+        System.out.println("Name is " + name);
+    }
+}
