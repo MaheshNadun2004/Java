122f04f9b5cbf9e520347e882e13a16514cb0c3d
Mahesh Nadun <mahesh.nandun2220@gmail.com>
2025-11-19 18:20:52 +0530

    java
    java

---
+++ p9.java
@@ -0,0 +1,12 @@
+public class p9{
+    public static void main(String[]args){
+        int day = 7;
+        int week = 3;
+        for(int i=0 ; i<=week ; i++){
+            System.out.println("  Week " + i);
+            for(int j=0 ; j<=day ; j++){
+                System.out.println("Day " + j);
+            }
+        }
+    }
+}
