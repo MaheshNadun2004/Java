f7f04dcf5eaff3ef6031e61d7e4f6ae0ec793268
Mahesh Nadun <mahesh.nandun2220@gmail.com>
2025-11-19 17:45:59 +0530

    java
    java

---
+++ p3.java
@@ -0,0 +1,7 @@
+public class p3{
+    public static void main(String[]args){
+        String name = "Mahesh";
+        String sName = "Nadun";
+        System.out.println(name + " " + sName);
+    }
+}
