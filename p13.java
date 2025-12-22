cd5bd47a5cc1c9440a39086fc05eaed88d404266
Mahesh Nadun <mahesh.nandun2220@gmail.com>
2025-12-22 22:27:45 +0530

    Create p13.java

---
+++ p13.java
@@ -0,0 +1,12 @@
+public class P1{
+	public static void main(String[] args){
+		String name = "Mahesh Nadun";
+		int number = 123;
+		char cha = 'A';
+		float pi = 12.43f;
+		
+		System.out.println(name);
+		System.out.println(cha);
+		System.out.println(pi);
+	}
+}
