1e728a3052f8bd2e58857c3f977d976f8a68fbc6
Mahesh Nadun <mahesh.nandun2220@gmail.com>
2025-12-22 22:23:24 +0530

    Add P1 class to print user details

---
+++ p12.java
@@ -0,0 +1,7 @@
+public class P1{
+	public static void main(String[] args){
+		System.out.println("Mahesh Nadun");
+		System.out.println("Anuradhapura");
+		System.out.println("070 43 44 282");
+	} 
+}
